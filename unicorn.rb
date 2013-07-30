6c498d400c1a0b4602b145299a208c73681ac076
Rob Miller <rob@bigfish.co.uk>
2013-07-30 17:14:54 +0100

    Unicorn config

---
+++ unicorn.rb
@@ -0,0 +1,13 @@
+worker_processes 2
+working_directory "/sites/zipper.bigfish.co.uk"
+
+preload_app true
+
+timeout 30
+
+listen "/tmp/zipper.sock", :backlog => 64
+
+pid "/tmp/zipper.pid"
+
+stderr_path "/sites/zipper.bigfish.co.uk/err.log"
+stdout_path "/sites/zipper.bigfish.co.uk/out.log"
